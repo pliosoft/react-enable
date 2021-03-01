0d3ced8023eb5f58a991da00a41e3fc7806faf9a
Christopher Lord <christopher@pliosoft.com>
2021-03-01 12:13:25 -0700

    prior to refactor

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,59 @@ import { insert, remove, elem, intersection, fromArray } from "fp-ts/lib/Set";
 import { eqString } from "fp-ts/lib/Eq";
 export { ToggleFeatures } from "./ToggleFeatures";
 
+type EnableContextType = (feature: string) => boolean;
+
+/**
+ * Contained function can check whether a given feature is enabled.
+ */
+export const EnableContext = React.createContext<EnableContextType>(
+  _s => false
+);
+
+// Helper: get rid of some boilerplate.
+// just input mashing and sanitation, removing extra renders, and getting test function
+function testAndConvert(input?: string | string[] | null) : [EnableContextType, string[]] {
+  if (Array.isArray(input) && input.length === 0 || input == null) {
+    throw new Error("Can't have empty array of features")
+  }
+  const test = React.useContext(EnableContext);
+  // We memoize just to prevent re-renders since this could be at the leaf of a tree
+  const converted = React.useMemo(() => Array.isArray(input) ? input : [input], [input])
+  return [test, converted]
+}
+
+/**
+ * returns true iff all specified features are enabled
+ */
+export function useAllEnabled(allFeatures: string | string[]) {
+  let [test, queryAllPresent] = testAndConvert(allFeatures)
+  return queryAllPresent.every(test)
+}
+
+/**
+ * returns true iff all specified features are disabled
+ */
+export function useAllDisabled(withoutAll: string | string[]) {
+  let [test, queryAllWithout] = testAndConvert(withoutAll)
+  return queryAllWithout.every(x => !test(x))
+}
+
+/**
+ * returns true iff any specified feature is enabled
+ */
+export function useEnabled(feature: string | string[]) {
+  let [test, queryAnyPresent] = testAndConvert(feature)
+  return queryAnyPresent.some(test)
+}
+
+/**
+ * returns true iff any specified feature is disabled
+ */
+export function useDisabled(without: string | string[]) {
+  let [test, queryAnyWithout] = testAndConvert(without)
+  return queryAnyWithout.some(x => !test(x))
+}
+
 interface EnableProps {
   readonly feature?: string | string[];
   readonly without?: string | string[];
@@ -10,81 +63,8 @@ interface EnableProps {
   readonly allFeatures?: string[];
   readonly withoutAll?: string[];
 }
-
-type EnableContextType = (feature: String) => boolean;
-
 /**
- * Provide a function that can be used to check whether a given feature is enabled.
- */
-export const EnableContext = React.createContext<EnableContextType>(
-  _s => false
-);
-
-/**
- * Given a feature name (or names), will determine if that feature (or any one) is enabled
- * Second argument allows for negation.
- */
-export function useEnabledFull(props: EnableProps): boolean {
-  const {
-    feature = [],
-    without = [],
-    allFeatures = [],
-    withoutAll = []
-  } = props;
-  const test = React.useContext(EnableContext);
-  if (test == null) {
-    return false;
-  }
-
-  let someFeatureList = Array.isArray(feature) ? feature : [feature];
-  let maybeEnabled =
-    someFeatureList.some(test) ||
-    (allFeatures.length > 0 && allFeatures.every(test));
-  if (!maybeEnabled && feature.length > 0) {
-    return false;
-  }
-
-  let someWithoutList = Array.isArray(without) ? without : [without];
-  let maybeDisabled =
-    someWithoutList.some(test) ||
-    (withoutAll.length > 0 && withoutAll.every(test));
-  if (maybeDisabled) {
-    return false;
-  }
-
-  return true;
-}
-
-/**
- * returns true iff all specified features are enabled
- */
-export function useAllEnabled(allFeatures: string[]) {
-  return useEnabledFull({ allFeatures });
-}
-
-/**
- * returns true iff all specified features are disabled
- */
-export function useAllDisabled(withoutAll: string[]) {
-  return useEnabledFull({ withoutAll });
-}
-
-/**
- * returns true iff any specified feature is enabled
- */
-export function useEnabled(feature: string | string[]) {
-  return useEnabledFull({ feature });
-}
-
-/**
- * returns true iff any specified feature is disabled
- */
-export function useDisabled(without: string | string[]) {
-  return useEnabledFull({ without });
-}
-
-/**
- * Feature will be enabled if any feature in the list are enabled, but without wins.
+ * Feature will be enabled if any feature in the list are enabled, unless some negative query also matches.
  * This allows you to enable things except if some other feature is active, for instance.
  */
 export const Enable: React.FC<EnableProps> = ({
@@ -94,7 +74,13 @@ export const Enable: React.FC<EnableProps> = ({
   withoutAll = [],
   children
 }) => {
-  if (useEnabledFull({ feature, without, allFeatures, withoutAll })) {
+  const isAny = useEnabled(feature);
+  const isAll = useAllEnabled(allFeatures);
+  const isNoAll = useAllDisabled(withoutAll);
+  const isNoAny = useDisabled(without);
+
+
+  if ((isAny || isAll) && !(isNoAll || isNoAny)) {
     return <>{children}</>;
   }
 
@@ -108,7 +94,9 @@ export interface Feature {
 
 interface EnableState {
   readonly features: Feature[];
-  readonly active: Set<string>;
+  readonly forceEnable: Set<string>;
+  readonly forceDisable: Set<string>;
+  readonly defaultActive: Set<string>;
 }
 
 interface DisableFeature {
@@ -121,6 +109,11 @@ interface ToggleFeature {
   readonly feature: string;
 }
 
+interface SetDefaultFeatures {
+  type: "set-default";
+  readonly defaultEnabled: string[];
+}
+
 interface EnableFeature {
   type: "enable";
   readonly feature: string;
@@ -128,13 +121,14 @@ interface EnableFeature {
 
 interface SetActiveFeatures {
   type: "set-active";
-  readonly active: string[];
+  readonly values: {on: string[], off: string[]};
 }
 
 type EnableAction =
   | EnableFeature
   | DisableFeature
   | ToggleFeature
+  | SetDefaultFeatures
   | SetActiveFeatures;
 
 const fromStrArray = fromArray(eqString);
@@ -144,14 +138,19 @@ const elemStr = elem(eqString);
 const intersectStr = intersection(eqString);
 
 const reducer: React.Reducer<EnableState, EnableAction> = (state, action) => {
+  // console.debug("REDUCE: \n++> %o\n++>%o\n ==> %o", state.forceDisable, state.forceDisable, action)
   switch (action.type) {
     case "enable": {
       if (!state.features.some(x => x.name === action.feature)) {
         return state;
       }
+
+			const enableAction = state.defaultActive.has(action.feature) ? removeStr(action.feature) : insertStr(action.feature)
+			const disableAction = removeStr(action.feature)
       return {
         ...state,
-        active: insertStr(action.feature)(state.active)
+        forceEnable:  enableAction(state.forceEnable),
+        forceDisable: disableAction(state.forceEnable)
       };
     }
 
@@ -161,7 +160,8 @@ const reducer: React.Reducer<EnableState, EnableAction> = (state, action) => {
       }
       return {
         ...state,
-        active: removeStr(action.feature)(state.active)
+        forceEnable: removeStr(action.feature)(state.forceEnable),
+        forceDisable: insertStr(action.feature)(state.forceEnable)
       };
     }
 
@@ -169,20 +169,26 @@ const reducer: React.Reducer<EnableState, EnableAction> = (state, action) => {
       if (!state.features.some(x => x.name === action.feature)) {
         return state;
       }
-      return {
-        ...state,
-        active: elemStr(action.feature, state.active)
-          ? removeStr(action.feature)(state.active)
-          : insertStr(action.feature)(state.active)
-      };
+
+      const current = elemStr(action.feature, state.defaultActive) ?
+        !elemStr(action.feature, state.forceDisable) :
+        elemStr(action.feature, state.forceEnable)
+
+      return reducer(state, {type: current ? 'disable' : 'enable', feature: action.feature})
+    }
+
+    case 'set-default': {
+        return {...state, defaultActive: fromStrArray(action.defaultEnabled)}
     }
 
     case "set-active": {
-      const proposedActive = fromStrArray(action.active);
+      const proposedOn = fromStrArray(action.values.on);
+      const proposedOff = fromStrArray(action.values.off);
       const possible = fromStrArray(state.features.map(x => x.name));
       return {
         ...state,
-        active: intersectStr(proposedActive, possible)
+        forceEnable: intersectStr(proposedOn, possible),
+        forceDisable: intersectStr(proposedOff, possible),
       };
     }
 
@@ -244,23 +250,34 @@ export const Features: React.FC<FeatureProps> = ({
   storage,
   children
 }) => {
-  const initial: EnableState = { features, active: new Set(defaultEnabled) };
+  const initial: EnableState = { features, forceDisable: fromStrArray([]), forceEnable: fromStrArray([]), defaultActive: fromStrArray(defaultEnabled) };
   const [state, dispatch] = React.useReducer(reducer, initial);
+  // console.debug("State: \n##> %o\n##> %o\n##> %o", state.forceEnable, state.forceDisable, state.defaultActive)
 
-  useLoadActive(storage, dispatch);
-  usePersistActive(state.active, storage);
+  useLoad(storage, dispatch);
+  usePersist(state.forceEnable, state.forceDisable, storage);
   useConsoleOverride(consoleOverride, dispatch);
+
+  React.useEffect(() => {
+    if (Array.isArray(defaultEnabled)) {
+      dispatch({ type: "set-default", defaultEnabled });
+    }
+  }, [dispatch, defaultEnabled])
 
   const featureValue = React.useMemo(() => ({ dispatch, state }), [
     dispatch,
     state,
-    state.active
+    state.defaultActive,
+    state.forceEnable,
+    state.forceDisable,
   ]);
 
-  const testCallback = React.useCallback((f: string) => state.active.has(f), [
-    state,
-    state.active
-  ]);
+  const testCallback = React.useCallback((f: string) => elemStr(f, state.forceEnable) ||
+      (elemStr(f, state.defaultActive) && !elemStr(f, state.forceDisable)),
+    [
+      state,
+      state.defaultActive, state.forceEnable, state.forceDisable
+    ]);
 
   return (
     <FeatureContext.Provider value={featureValue}>
@@ -275,6 +292,7 @@ function useConsoleOverride(
   consoleOverride: boolean,
   dispatch: React.Dispatch<EnableAction>
 ) {
+
   React.useEffect(() => {
     if (!consoleOverride) {
       return () => {
@@ -286,9 +304,10 @@ function useConsoleOverride(
       window.feature = undefined;
     };
   }, [dispatch]);
+
 }
 
-function useLoadActive(
+function useLoad(
   storage: Storage | undefined,
   dispatch: React.Dispatch<EnableAction>
 ) {
@@ -297,7 +316,10 @@ function useLoadActive(
       try {
         const value = storage.getItem("react-enable:features");
         if (value != null) {
-          dispatch({ type: "set-active", active: JSON.parse(value) });
+          const result = JSON.parse(value);
+          if (Array.isArray(result.on) && Array.isArray(result.off) && typeof result.on[0] === "string" && typeof result.off[0] === "string" ) {
+            dispatch({ type: "set-active", values: result });
+          }
         }
       } catch (e) {
         // Can't parse or get
@@ -306,9 +328,10 @@ function useLoadActive(
   }, [storage]);
 }
 
-function usePersistActive(active: Set<string>, storage: Storage | undefined) {
+function usePersist(forceEnable: Set<string>, forceDisable: Set<string>, storage: Storage | undefined) {
   const strState =
-    storage == null ? undefined : JSON.stringify(Array.from(active).sort());
+    storage == null ? undefined : JSON.stringify({on: Array.from(forceEnable).sort(), off: Array.from(forceDisable).sort()});
+
   React.useEffect(() => {
     if (strState != null && storage != null) {
       try {
