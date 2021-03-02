1d1479f9a4c1b614e84be371ba7e0814afcb503a
Christopher Lord <christopher@pliosoft.com>
2021-03-01 18:41:00 -0700

    don't throw for empty list

--- src/utils.ts
+++ src/utils.ts
@@ -6,13 +6,10 @@ import { EnableContextType, EnableContext } from "./EnableContext";
 export function testAndConvert(
   input?: string | string[] | null
 ): [EnableContextType, string[]] {
-  if ((Array.isArray(input) && input.length === 0) || input == null) {
-    throw new Error("Can't have empty array of features");
-  }
   const test = React.useContext(EnableContext);
   // We memoize just to prevent re-renders since this could be at the leaf of a tree
   const converted = React.useMemo(
-    () => (Array.isArray(input) ? input : [input]),
+    () => (input == null ? [] : (Array.isArray(input) ? input : [input])),
     [input]
   );
   return [test, converted];
